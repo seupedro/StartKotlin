767667dacf10bb153a7e037cb0a828cc539da0ff
nakamoto <hartmnn.p@gmail.com>
2018-09-07 18:26:31 -0300

    Add count logic

--- app/src/main/java/io/github/sshnakamoto/startkotlin/MainActivity.kt
+++ app/src/main/java/io/github/sshnakamoto/startkotlin/MainActivity.kt
@@ -2,7 +2,8 @@ package io.github.sshnakamoto.startkotlin
 
 import android.app.Activity
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : Activity() {
 
@@ -11,5 +12,20 @@ class MainActivity : Activity() {
         setContentView(R.layout.activity_main)
 
     }
+
+    /*
+    * Get view value and add plus one
+    * */
+    fun count(view: View){
+        val showCount = findViewById<TextView>(R.id.count_view)
+        val countString = showCount.text.toString()
+
+        var count: Int = Integer.parseInt(countString)
+        count++
+
+        showCount.setText(count.toString())
+    }
+
+
 
 }
